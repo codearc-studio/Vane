5a5c4f6d7ce2dd68b1b1ed8b60c996208e0d9686
CodeArc.studio <contact@codearc.studio>
2026-08-13 12:37:18 -0400

    Initial Commit

---
+++ Vane/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Vane
+//
+//  Created by Makai O'Neill on 8/13/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Vane/VaneApp.swift
@@ -0,0 +1,32 @@
+//
+//  VaneApp.swift
+//  Vane
+//
+//  Created by Makai O'Neill on 8/13/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VaneApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
